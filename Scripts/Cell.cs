176904bdde5a9623d969374d84a8bf6be42bd226
davidmorillacabello <davidmorillacabello@gmail.com>
2020-04-06 20:06:26 +0200

    First version

---
+++ Scripts/Cell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace nsMaze
+{
+    public class Cell
+    {
+        //Variable to store if the current cell was used.
+        public bool isUsed;
+        // Variable to store if the cell is connected to any other and its direction: [N E S W]
+        public bool[] connected;
+        public Cell()
+        {
+            //Initialized the cell as not used and not connected.
+            isUsed = false;
+            connected = new bool[4] { false, false, false, false };
+        }
+    }
+}
